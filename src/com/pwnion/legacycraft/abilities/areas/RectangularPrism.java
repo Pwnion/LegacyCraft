c738abf4e530dd103a07a6989d2b7a23c8203ebb
Zephreo <hdentry56@gmail.com>
2020-02-01 20:47:23 +1100

    Allow Export Designs

--- src/com/pwnion/legacycraft/abilities/areas/RectangularPrism.java
+++ src/com/pwnion/legacycraft/abilities/areas/RectangularPrism.java
@@ -2,6 +2,7 @@ package com.pwnion.legacycraft.abilities.areas;
 
 import java.util.ArrayList;
 
+import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 
@@ -14,6 +15,28 @@ public class RectangularPrism {
 		for(int distance = 0; distance < height; distance++) {
 			for(Block squareBlock : Square.get(block.getRelative(BlockFace.UP, distance), radius)) {
 				rectangularPrism.add(squareBlock);
+			}
+		}
+		
+		return rectangularPrism;
+	}
+	
+	public static final ArrayList<Block> get(Block block1, Block block2) {
+		if(block1.getWorld() != block2.getWorld()) {
+			return null;
+		}
+		ArrayList<Block> rectangularPrism = new ArrayList<Block>();
+		
+		Location pos1 = block1.getLocation();
+		Location pos2 = RotateArea.getRelativeLoc(block1.getLocation(), block2.getLocation());
+		Location temp;
+		
+		for(int x = 0; x <= pos2.getBlockX(); x++) {
+			for(int y = 0; y <= pos2.getBlockY(); y++) {
+				for(int z = 0; z <= pos2.getBlockZ(); z++) {
+					temp = new Location(block1.getWorld(), x, y, z);
+					rectangularPrism.add(temp.getBlock());
+				}
 			}
 		}
 		
---
+++ src/com/pwnion/legacycraft/abilities/areas/Selection.java
@@ -0,0 +1,51 @@
+package com.pwnion.legacycraft.abilities.areas;
+
+import java.util.HashMap;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
+
+import com.pwnion.legacycraft.ConfigAccessor;
+
+public class Selection {
+    private static final ConfigAccessor structuresConfig = new ConfigAccessor("structures.yml");
+    private static final ConfigurationSection structuresCS = structuresConfig.getRoot();
+    
+    @SuppressWarnings("unchecked")
+    public static final HashMap<Location, Material> load(String name) {
+        return (HashMap<Location, Material>) structuresCS.get("structures." + name);
+    }
+    
+    private Player p;
+    private Block pos1;
+    private Block pos2;
+    
+    Selection(Player p) {
+        this.p = p;
+    }
+    
+    public final void setPos1() {
+        pos1 = p.getTargetBlock(null, 100);
+    }
+    
+    public final void setPos2() {
+        pos2 = p.getTargetBlock(null, 100);
+    }
+    
+    public final void export(String name) {
+        HashMap<Location, Material> data = new HashMap<Location, Material>();
+        if(pos1 == null || pos2 == null) {
+            return;
+        } else {
+            //add to data (i.e. data.put(key, value))
+        	for(Block block : RectangularPrism.get(pos1, pos2)) {
+        		data.put(block.getLocation(), block.getType());
+        	}
+        }
+        structuresCS.set("structures." + name, data);
+        structuresConfig.saveCustomConfig();
+    }
+}
