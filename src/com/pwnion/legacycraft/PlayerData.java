de6a43401ceb476d5fa73ecf4568b6a54d83af38
Harry <hdentry56@gmail.com>
2020-08-03 10:41:37 +1000

    playerData comment

--- src/com/pwnion/legacycraft/PlayerData.java
+++ src/com/pwnion/legacycraft/PlayerData.java
@@ -12,5 +12,65 @@ public enum PlayerData {
 	FINISHED_QUESTS,
 	SWAP_SLOT,
 	EXPERIENCE,
-	LAST_ATTACK
+	LAST_ATTACK;
+	
+	/* Possible replacement to PlayerData
+	 
+	public enum PlayerDataType {
+		Test(new PlayerDataStore<Integer>(1));
+
+		private PlayerDataStore<?> defaultt;
+	 
+	 	PlayerDataType(PlayerDataStore<?> defaultt) {
+	 		this.defaultt = defaultt;
+	 	}
+	 	
+	 	public PlayerDataStore<?> getDefault() {
+	 		return defaultt;
+	 	}
+	 }
+	 
+	 private class PlayerDataStore<T> {
+	 	private T data;
+	 	
+	 	public PlayerDataStore(T data) {
+	 		this.data = data;
+	 	}
+	 	
+	 	public T get() {
+	 		return data;
+	 	}
+	 	
+	 	public void set(T data) {
+	 		this.data = data;
+	 	}
+	 	
+	 	public PlayerDataStore<T> clone() {
+	 		return new PlayerDataStore<T>(data);
+	 	}
+	 }
+	 
+	 public class PlayerData {
+	 	private static HashMap<UUID, HashMap<PlayerDataType, PlayerDataStore<?>>> playerData = new HashMap<UUID, HashMap<PlayerDataType, PlayerDataStore<?>>>();
+	 	
+	 	public static PlayerDataStore<?> get(UUID playerUUID, PlayerDataType type) {
+	 		return playerData.get(playerUUID).get(type);
+	 	}
+	 	
+	 	public static HashMap<PlayerDataType, PlayerDataStore<?>> get(UUID playerUUID) {
+	 		return playerData.get(playerUUID);
+	 	}
+	 	
+	 	public static void generate(UUID playerUUID) {
+	 		for(PlayerDataType type : PlayerDataType.values()) {
+	 			HashMap<PlayerDataType, PlayerDataStore<?>> data = new HashMap<PlayerDataType, PlayerDataStore<?>>();
+	 			data.put(type, type.getDefault().clone());
+	 			playerData.put(playerUUID, data);
+	 		}
+	 	}
+	 }
+	 
+	 //No Casting!
+	 Integer lastAttack = PlayerData.get(playerUUID, PlayerDataType.LAST_ATTACK).get();
+	//*/
 }
